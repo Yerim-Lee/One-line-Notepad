671c2d425c06ddd1dae8c39a1718819fd51d829f
Yerim-Lee <cocoa09100@nate.com>
2020-11-24 00:12:32 +0900

    Revert "no message"
    This reverts commit a57db935a8a3ec1adab967eab1b9af824c00a62b.

--- config/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    name='방문자'
-    msg='안녕하세요.'
-    return render(request, 'memo/index.html',
-                  {'name':name, 'msg':msg})
